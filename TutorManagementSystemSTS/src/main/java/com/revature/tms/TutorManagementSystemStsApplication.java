63fb0168e80c40af8457afda631f03fca5b969ff
kileader <kevin.i.leader@gmail.com>
2021-08-05 13:14:53 -0400

    Add starter spring project for backend

---
+++ TutorManagementSystemSTS/src/main/java/com/revature/tms/TutorManagementSystemStsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorManagementSystemStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorManagementSystemStsApplication.class, args);
+	}
+
+}
---
+++ TutorManagementSystemSTS/src/test/java/com/revature/tms/TutorManagementSystemStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorManagementSystemStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
