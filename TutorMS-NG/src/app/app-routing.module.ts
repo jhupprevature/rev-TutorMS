cef002549ba930a254696a1a56c976bb9626c8ef
jhupprevature <jordan.hupp@revature.net>
2021-08-06 10:10:58 -0400

    Adding profile component to navition

--- TutorMS-NG/src/app/app-routing.module.ts
+++ TutorMS-NG/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HoursSummaryComponent } from './components/summary/hours-summary/hours-
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'summary', component: HoursSummaryComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
