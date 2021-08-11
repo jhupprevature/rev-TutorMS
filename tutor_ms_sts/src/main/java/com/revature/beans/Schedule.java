9fa562984a45ceadafd8c58463ab469d95abb05b
kileader <kevin.i.leader@gmail.com>
2021-08-10 22:17:39 -0400

    Remove generatedValue from Schedule

--- tutor_ms_sts/src/main/java/com/revature/beans/Schedule.java
+++ tutor_ms_sts/src/main/java/com/revature/beans/Schedule.java
@@ -4,8 +4,6 @@ import java.time.LocalTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -15,7 +13,6 @@ import javax.persistence.Table;
 public class Schedule {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tutor_id", updatable = false)
     private int id;
     
