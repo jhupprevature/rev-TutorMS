a9fa564d8ec71f13a8992ea50a8299a17f16e1bf
caocmai <caomai@rocketmail.com>
2021-08-14 19:09:51 -0700

    Minor styling fixes

--- TutorMS-NG/src/app/app.module.ts
+++ TutorMS-NG/src/app/app.module.ts
@@ -52,7 +52,6 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     SchedulingComponent,
     ProfileComponent,
     AnalyticsComponent,
-    ProfileComponent,
     CardComponent,
     LoginComponent,
     RegistrationComponent,
