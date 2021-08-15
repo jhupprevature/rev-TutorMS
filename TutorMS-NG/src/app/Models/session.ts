37859a4d4ac9d8259c247a9bbf16dd9adb9a3ca2
jhupprevature <jordan.hupp@revature.net>
2021-08-15 17:43:26 -0400

    refactor for SessionStub model

--- TutorMS-NG/src/app/Models/session.ts
+++ TutorMS-NG/src/app/Models/session.ts
@@ -1,26 +1,26 @@
 import { Course } from "./Course";
 import { User } from "./User";
 
-// export class Session {
-//   userId: number;
-//   studentId: number;
-//   courseId: number;
-//   startTime: number;
-//   endTime: number;
-//   studentNotes: string;
-//   tutorNotes: string;
+export class SessionStub {
+  userId: number;
+  studentId: number;
+  courseId: number;
+  startTime: number;
+  endTime: number;
+  studentNotes: string;
+  tutorNotes: string;
 
-//   constructor(userId: number, studentId: number, courseId: number, startTime: number, endTime: number, studentNotes: string, tutorNotes: string){
-//     this.userId = userId;
-//     this.studentId = studentId;
-//     this.courseId = courseId;
-//     this.startTime = startTime;
-//     this.endTime = endTime;
-//     this.studentNotes = studentNotes;
-//     this.tutorNotes = tutorNotes;
+  constructor(userId: number, studentId: number, courseId: number, startTime: number, endTime: number, studentNotes: string, tutorNotes: string){
+    this.userId = userId;
+    this.studentId = studentId;
+    this.courseId = courseId;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.studentNotes = studentNotes;
+    this.tutorNotes = tutorNotes;
 
-//   }
-// }
+  }
+}
 
 export class Session {
   id: number;
